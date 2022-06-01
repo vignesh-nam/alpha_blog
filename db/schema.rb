1cdd9f68290f076d95a0d8c1c48f6eb0d899b6c9
Vignesh <vigneshbabu10@gmail.com>
2022-06-01 15:36:46 +0530

    Create Migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_01_091532) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_01_095319) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
